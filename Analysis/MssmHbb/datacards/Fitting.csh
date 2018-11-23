b3f2685e493ef1bbfcb29e939aa716ba6dedcc7d
Roberval Walsh <roberval.walsh@gmail.com>
2018-11-23 15:21:02 +0100

    deleted other packages

--- Analysis/MssmHbb/datacards/Fitting.csh
+++
@@ -1,1 +0,0 @@
-combine -M MaxLikelihoodFit --plots --saveNormalizations --saveShapes --saveWithUncertainties --saveNLL --robustFit=1 --rMin=0 --rMax=2 -m 81 $1 
--- Analysis/MssmHbb/python/lib/makeRootHistos.cpp
+++
@@ -1,8 +0,0 @@
-#include <string>;
-
-using namespace std;
-
-void makeRootHistos(){
-	string inputTxt = "/nfs/dust/cms/user/shevchen/SusHiScaner/output/test1/txtFiles/type3_mA.txt";
-	TFile f(inputTxt.c_str(),"read");
-}
