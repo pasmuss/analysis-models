407bdb92722b40eb1b65d6874e92eecac361ae7f
Roberval Walsh <roberval.walsh@gmail.com>
2018-11-28 11:15:58 +0100

    backward compatibility with analysis::backgroundmodel

---
+++ src/RooGausExp_2016.cc
@@ -0,0 +1,72 @@
+#include <cmath>
+#include "Riostream.h"
+#include "RooAbsReal.h"
+#include "RooAbsCategory.h"
+#include "TMath.h"
+#include "Analysis/Models/interface/2016/RooGausExp.h"
+
+
+using namespace analysis::backgroundmodel;
+
+
+ClassImp(RooGausExp)
+
+
+RooGausExp::RooGausExp(const char *name, const char *title,
+                             RooAbsReal& x,
+                             RooAbsReal& mean,
+                             RooAbsReal& sigma,
+                             RooAbsReal& tail_shift,
+							 RooAbsReal& tail_sigma) :
+  RooAbsPdf(name, title),
+  x_("x", "x", this, x),
+  mean_("mean", "mean", this, mean),
+  sigma_("sigma", "sigma", this, sigma),
+  tail_shift_("tail_shift", "tail_shift", this, tail_shift),
+  tail_sigma_("tail_sigma", "tail_sigma", this, tail_sigma){
+}
+
+
+RooGausExp::RooGausExp(const RooGausExp& other, const char* name) :
+  RooAbsPdf(other, name),
+  x_("x", this, other.x_),
+  mean_("mean", this, other.mean_),
+  sigma_("sigma", this, other.sigma_),
+  tail_shift_("tail_shift", this, other.tail_shift_),
+  tail_sigma_("tail_sigma", this, other.tail_sigma_){
+//  mean_("mean", this, other.mean_),
+//  sigma_("sigma", this, other.sigma_),
+//  right_("right", this, other.right_) {
+}
+
+
+TObject* RooGausExp::clone(const char* newname) const {
+  return new RooGausExp(*this, newname);
+}
+
+
+double RooGausExp::evaluate() const {
+//  double std = (x_ - mean_) / sigma_;
+//  double result = 0.0;
+//
+//  if (std <= right_) {
+//    result = std::exp(-0.5 * std::pow(std, 2));
+//  } else {
+//    result = std::exp(0.5*right_*right_ - right_*std);
+//  }
+
+	double result = 0;
+	double a1 = (x_ - mean_) / sigma_;
+	double a2 = (x_ - tail_shift_) / tail_sigma_;
+	double g1 = TMath::Exp(-0.5 * a1 * a1);
+	double b1 = (tail_shift_ - mean_) / sigma_;
+	double g1_shift = TMath::Exp(-0.5 * b1 * b1);
+	double exp = 1. / g1_shift * TMath::Exp(-0.5 * a2);
+
+	if(x_ < tail_shift_) result = g1;
+	else result = exp;
+	return result;
+}
+
+
+
--- src/classes.h
+++ src/classes.h
@@ -12,4 +12,22 @@
 #include "Analysis/Models/interface/RooSuperNovosibirsk.h"
 #include "Analysis/Models/interface/RooPolyDijet.h"
 #include "Analysis/Models/interface/RooSuperDiJet.h"
-#include "Analysis/Models/interface/RooBackgroundModel.h"  // for backward compatibility
+
+
+//  for backward compatibility
+//  don´t add anything below
+//  eventually remove all that
+#include "Analysis/Models/interface/2016/RooDoubleCB.h"
+#include "Analysis/Models/interface/2016/RooExpGausExp.h"
+#include "Analysis/Models/interface/2016/RooGausExp.h"
+#include "Analysis/Models/interface/2016/RooExpBWExp.h"
+#include "Analysis/Models/interface/2016/RooPhaseSpace.h"
+#include "Analysis/Models/interface/2016/RooPhaseSpacePol4.h"
+#include "Analysis/Models/interface/2016/RooRelBreitWigner.h"
+#include "Analysis/Models/interface/2016/RooQuadGausExp.h"
+#include "Analysis/Models/interface/2016/RooMyNovosibirsk.h"
+#include "Analysis/Models/interface/2016/RooExtendNovosibirsk.h"
+#include "Analysis/Models/interface/2016/RooDoubleGausExp.h"
+#include "Analysis/Models/interface/2016/RooSuperNovosibirsk.h"
+#include "Analysis/Models/interface/2016/RooPolyDijet.h"
+#include "Analysis/Models/interface/2016/RooSuperDiJet.h"
